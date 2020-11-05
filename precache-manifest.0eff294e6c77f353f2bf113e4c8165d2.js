8eeb6c35ff1b26a4be2eb76cf12d3bdff5a779c5
Talmage <talmagelucas@gmail.com>
2020-11-04 17:55:36 -0700

    Updates

---
+++ precache-manifest.0eff294e6c77f353f2bf113e4c8165d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90efd6e72651466f27c5b49da0ed400",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "673c4bda7a36f931122c",
+    "url": "/react-employee-directory/static/css/main.c6ca589f.chunk.css"
+  },
+  {
+    "revision": "3fa83f6aeda0fb34a3ac",
+    "url": "/react-employee-directory/static/js/2.136dd58d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-employee-directory/static/js/2.136dd58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673c4bda7a36f931122c",
+    "url": "/react-employee-directory/static/js/main.d5a09582.chunk.js"
+  },
+  {
+    "revision": "4fb9de5ca140f3b8d25e",
+    "url": "/react-employee-directory/static/js/runtime-main.b0c0ef81.js"
+  },
+  {
+    "revision": "331b94e8efb70875a459b45f34be6634",
+    "url": "/react-employee-directory/static/media/logo.331b94e8.png"
+  }
+]);
