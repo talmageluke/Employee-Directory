3348732f6df5d434200e39e994c331ac0261d000
Talmage <talmagelucas@gmail.com>
2020-11-04 18:00:14 -0700

    Updates

---
+++ precache-manifest.76d9374af91b3cdc269691a385ebcb0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e930cfa4a96694b43dcff2a9560fb7fb",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "b0599c4d66b8032caa5f",
+    "url": "/Employee-Directory/static/css/main.c6ca589f.chunk.css"
+  },
+  {
+    "revision": "b944f776e4030ef54418",
+    "url": "/Employee-Directory/static/js/2.d06d2251.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Employee-Directory/static/js/2.d06d2251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0599c4d66b8032caa5f",
+    "url": "/Employee-Directory/static/js/main.a5657165.chunk.js"
+  },
+  {
+    "revision": "2fd62fc6413e7473f99c",
+    "url": "/Employee-Directory/static/js/runtime-main.e973b803.js"
+  },
+  {
+    "revision": "331b94e8efb70875a459b45f34be6634",
+    "url": "/Employee-Directory/static/media/logo.331b94e8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-employee-directory/precache-manifest.6d7e4a3f8ea963d36f372b56ccc48e08.js"
+  "/Employee-Directory/precache-manifest.76d9374af91b3cdc269691a385ebcb0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-employee-directory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Employee-Directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
